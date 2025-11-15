c8a9bb94bdd781459085bef6208fee7bf035a6f2
tarakanovandrej1635 <tarakanovandrej1635@gmail.com>
2025-11-15 17:52:08 +0300

    Девилам

---
+++ js/accordeon.js
@@ -0,0 +1,10 @@
+const showAccordeon = () => {
+    const accordeon = document.querySelector('.news__page__main__news__bibliography__accordeon')
+
+    accordeon.addEventListener('click', ()=> {
+        accordeon.nextElementSibling.classList.toggle('accordeon__active')
+        accordeon.classList.toggle('accordeon__rotate')
+    })
+}
+
+showAccordeon()
